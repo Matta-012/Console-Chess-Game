12e8797720657548f9bc03e83cc3648a5b5d450e
Joao Marco <joaomarco.matta@gmail.com>
2021-01-20 11:24:11 -0300

    Criando a classe PosicaoXadrez.

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace ConsoleChessGame
     {
         static void Main(string[] args)
         {
-            try
+            /*try
             {
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
@@ -21,7 +21,12 @@ namespace ConsoleChessGame
             catch (TabuleiroException e)
             {
                 Console.WriteLine(e.Message);
-            }
+            }*/
+
+            PosicaoXadrez pos = new PosicaoXadrez('c', 7);
+
+            Console.WriteLine(pos);
+            Console.WriteLine(pos.ToPosicao());
 
 
         }
